2a28e5a668930e42f5d7c76b0d78493db57ad627
mike <mikebatani@outlook.com>
2018-09-16 17:41:35 -0700

    adding a new file

---
+++ FileIO.java
@@ -0,0 +1,34 @@
+import java.io.* ;
+import java.util.Scanner;
+public class FileIO
+{
+  public static void main(String[] args)  throws IOException
+  {
+    //This is to creat a file appand and write to it 
+     //to get the file name
+    Scanner userInput= new Scanner (System.in);
+    System.out.println("Enter the name of the file you want to creat");
+    String fileName= userInput.nextLine();
+    
+    //To creat a object out of fileWriter to be able to appand and creat a print wrier to be able to use println methods
+      
+    FileWriter fWriter= new FileWriter(fileName, true);
+    PrintWriter outputFile = new PrintWriter(fWriter); //opening the File 
+    
+    System.out.println("Howmany friends do you have");
+    //new int variable
+    int numFriend = userInput.nextInt();
+      String friendName;
+      for (int i = 1; i <= numFriend; i++)
+    {
+          System.out.println("what is the friend's " + i +"  name:");
+           friendName = userInput.nextLine();
+          userInput.nextLine();
+          
+      outputFile.println(friendName);
+    }
+     //just have to close the PrintWriter Class
+      outputFile.close();
+          System.out.println("data has been saved.");
+  }
+}
